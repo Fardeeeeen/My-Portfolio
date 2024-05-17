c078a7c395a6f378e4829cfdc64fc089a56a2769
Fardeen <fardeenzubair@gmail.com>
2024-05-17 18:29:44 +0530

    live

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,30 +22,35 @@ export const Projects = () => {
       description: "The Notebook is a full-stack web application inspired by Google keep app, which aims at providing users with a versatile platform to manage their notes and drawings efficiently. It offers functionalities like creating, editing, archiving, and deleting notes, as well as creating, downloading and deleting drawings.",
       imgUrl: projImg4,
       source:"https://github.com/Fardeeeeen/NoteBook",
+      live:"https://notebook-frontend-a92q.onrender.com",
     },
      {
       title: "Book Library",
       description: "The Book Library is a web application designed to help users manage their book collections effectively. It allows users to view, add, edit, and delete books. Users can add notes, summaries, and ratings for each book, enhancing their reading experience.The application integrates with the Open Library Covers API to fetch book covers and provides a seamless experience for users.",
       imgUrl: projImg3,
       source:"https://github.com/Fardeeeeen/Book-Library",
+      live:"https://book-library-k4wa.onrender.com",
     },
      {
       title: "My Portfolio",
       description: "It is a React.js-powered portfolio, a showcase of my web development prowess and projects. As you explore this very portfolio, you're witnessing firsthand the skills and dedication poured into its creation. ",
       imgUrl: projImg8,
-      source:"https://github.com/Fardeeeeen/NoteBook",
+      source:"https://github.com/Fardeeeeen/My-Portfolio",
+      live:"http://www.fardeenzubair.com",
     },
      {
       title: "BlogApp",
       description: "The Blog App is a web application designed to allow users to create, edit, and manage their blog posts. It provides an intuitive interface for bloggers to share their thoughts, experiences, and stories with the world.",
       imgUrl: projImg2,
       source:"https://github.com/Fardeeeeen/Blog-App",
+      live:"https://blog-app-x5pp.onrender.com"
     },
     {
       title: "Lets Mix",
       description: " Let's Mix is a dynamic web application designed to provide users with instant access to a vast collection of cocktail recipes. With a simple and intuitive interface, users can explore a diverse range of cocktail options and discover new favorites with ease. Powered by an external API, Let's Mix brings the joy of mixology to users worldwide.",
       imgUrl: projImg1,
       source:"https://github.com/Fardeeeeen/Lets-Mix",
+      live:"https://lets-mix.onrender.com",
     },
    
     {
@@ -53,18 +58,21 @@ export const Projects = () => {
       description: "A Web 3.0 website in which i created a crypto Token of my own called Dragonballs and users can acquire them on this website. ",
       imgUrl: projImg7,
       source:"https://github.com/Fardeeeeen/DbRadar",
+      live:"https://github.com/Fardeeeeen/DbRadar",
     },
      {
       title: "DBank",
       description: "A Web 3.0 website in which a user can deposit or transfer crypto web tokens and coins",
       imgUrl: projImg6,
       source:"https://github.com/Fardeeeeen/DBank",
+      live:"https://github.com/Fardeeeeen/DBank",
     },
      {
       title: "Simon-Game",
       description: " The Simon Game Challenge is a web application where users can play the classic Simon game online. Simon is an electronic game of short-term memory skill, invented by Ralph H. Baer and Howard J. Morrison. The game creates a series of tones and lights and requires the user to repeat the sequence.",
       imgUrl: projImg5,
       source:"https://github.com/Fardeeeeen/Simon-Game",
+      live:"https://fardeeeeen.github.io/Simon-Game/",
     },
   ];
 
