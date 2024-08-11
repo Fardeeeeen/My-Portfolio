b0543e8f184f690d8ef786aac506a010c3283678
Fardeen <fardeenzubair@gmail.com>
2024-08-11 14:01:17 +0530

    newsletter removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
+import { Container,Col } from "react-bootstrap";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -10,8 +9,6 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Row className="align-items-center">
-          <MailchimpForm />
           <Col>
            <p>Copyright 2024. All Rights Reserved</p>
           </Col>
@@ -23,7 +20,6 @@ export const Footer = () => {
             <a href="https://github.com/Fardeeeeen"><img src={navIcon4} alt="" /></a>
             </div>
           </Col>
-        </Row>
       </Container>
     </footer>
   );
